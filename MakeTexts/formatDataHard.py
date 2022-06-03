f48734b394f267dd84b01a1517412af09cef4ece
Guilherme Oliveira <51828246+Guimol@users.noreply.github.com>
2022-06-03 09:31:30 -0300

    Update formatDataHard.py

--- MakeTexts/formatDataHard.py
+++ MakeTexts/formatDataHard.py
@@ -14,16 +14,20 @@ texto = data.read()
 capitulos = ['PRIMEIRO', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI', 'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L', 'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LIX', 'LX', 'LXI', 'LXII', 'LXIII', 'LXIV', 'LXV', 'LXVI', 'LXVII', 'LXVIII', 'LXIX', 'LXX', 'LXXI', 'LXXII', 'LXXIII', 'LXXIV', 'LXXV', 'LXXVI', 'LXXVII', 'LXXVIII', 'LXXIX', 'LXXX', 'LXXXI', 'LXXXII', 'LXXXIII', 'LXXXIV', 'LXXXV', 'LXXXVI', 'LXXXVII', 'LXXXVIII', 'LXXXIX', 'XC', 'XCI', 'XCII', 'XCIII', 'XCIV', 'XCV', 'XCVI', 'XCVII', 'XCVIII', 'XCIX', 'C', 'CI', 'CII', 'CIII', 'CIV', 'CV', 'CVI', 'CVII', 'CVIII', 'CIX', 'CX', 'CXI', 'CXII', 'CXIII', 'CXIV', 'CXV', 'CXVI', 'CXVII', 'CXVIII', 'CXIX', 'CXX', 'CXXI', 'CXXII', 'CXXIII', 'CXXIV', 'CXXV', 'CXXVI', 'CXXVII', 'CXXVIII', 'CXXIX', 'CXXX', 'CXXXI', 'CXXXII', 'CXXXIII', 'CXXXIV', 'CXXXV', 'CXXXVI', 'CXXXVII', 'CXXXVIII', 'CXXXIX', 'CXL', 'CXLI', 'CXLII', 'CXLIII', 'CXLIV', 'CXLV', 'CXLVI', 'CXLVII', 'CXLVIII', 'CXLIX', 'CL', 'CLI', 'CLII', 'CLIII', 'CLIV', 'CLV', 'CLVI', 'CLVII', 'CLVIII', 'CLIX', 'CLX', 'CLXI', 'CLXII', 'CLXIII', 'CLXIV', 'CLXV', 'CLXVI', 'CLXVII', 'CLXVIII', 'CLXIX', 'CLXX', 'CLXXI', 'CLXXII', 'CLXXIII', 'CLXXIV', 'CLXXV', 'CLXXVI', 'CLXXVII', 'CLXXVIII', 'CLXXIX', 'CLXXX', 'CLXXXI', 'CLXXXII', 'CLXXXIII', 'CLXXXIV', 'CLXXXV', 'CLXXXVI', 'CLXXXVII', 'CLXXXVIII', 'CLXXXIX', 'CXC', 'CXCI', 'CXCII', 'CXCIII', 'CXCIV', 'CXCV', 'CXCVI', 'CXCVII', 'CXCVIII', 'CXCIX', 'CC', 'CCI']
 
 livro = {}
+count = 0
 for capitulo in capitulos:
     textoNaoFormatado = texto.split('CAPITULO')[capitulos.index(capitulo) + 1].strip()
     textoParagrafado = textoNaoFormatado.split(capitulo)[-1].strip()
     livro['CAPITULO ' + capitulo] = ' '.join(textoParagrafado.split('\n'))
     
+    count += 1
+    
 from nltk.tokenize import word_tokenize
 
 for key in livro:
     words = word_tokenize(livro[key])
-    new_words = [value for value in words if value not in ('--', '_', '-')]
+    new_words = [value for value in words if value not in ('--', '_', '-', '–', '<<', '>>', '«', '»')] #Tokens not to include in hard version
+    #new_words = [value for value in words if value not in ('--', '_', '-', '–', '``', '\'\'')] Tokens not to include in easy version
     import string
     formattedText = ''
     flag = True
@@ -55,7 +59,13 @@ for key in copy_livro:
     if size > 150 or size < 51:
         del(livro[key])
 
-json_data = {'Quincas Borbas': livro}
+json_data = {'book': {'title': 'Quincas Borbas'}}
+
+for index, key in enumerate(livro):
+    json_data['book'][index] = {}
+    json_data['book'][index]['chapter'] = key
+    json_data['book'][index]['text'] = livro[key]
+
 
 with open('QuincasBorbasShortHard.json', 'w', encoding='utf8') as json_file:
     json.dump(json_data, json_file, ensure_ascii=False)
