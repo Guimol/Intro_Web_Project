e41d0906a7f218714be76c649d980a7ab21a2bd7
Guimol <guizera11@hotmail.com.br>
2022-06-08 06:47:59 -0300

    1 file modified

--- src/components/get_list_tokens.vue
+++ src/components/get_list_tokens.vue
@@ -15,11 +15,35 @@ export default {
   data() {
     return {
       list_tokens: [],
+      difficulty: 'Hard',
+      array: [],
+      array_index: 0,
     };
   },
   methods: {
     iterate_counter() {
       this.count = this.count + 1;
+    },
+  },
+  computed: {
+    nextList() {
+      if (this.array_index < this.list_tokens.length - 10) {
+        this.array = this.list_tokens.slice(
+          this.array_index,
+          this.array_index + 10
+        );
+      }
+      this.array_index++;
+    },
+
+    showList() {
+      if (this.difficulty == 'Hard') {
+        this.list_tokens = hard_json.book[this.count].text.split(' ');
+      } else {
+        this.list_tokens = easy_json.book[this.count].text.split(' ');
+      }
+
+      return this.list_tokens;
     },
   },
 };
@@ -48,14 +72,14 @@ export default {
   </button>
   <p>{{ hard_json.book.title }}</p>
   <p>{{ hard_json.book[count].chapter }}</p>
-  <p>{{ hard_json.book[count].text }}</p>
+  <!--<p>{{ hard_json.book[count].text.split(' ') }}</p>-->
+  <p>{{ showList }}</p>
+  <button type="button" @click="nextList">Next List</button>
+  <p>{{ array }}</p>
+  <p>Indice {{ array_index }}</p>
   <button type="button" @click="count--">Previous Chapter</button>
   <button type="button" @click="count = 0">Reset Chapter index</button>
   <!--TODO: Check last chapter-->
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
