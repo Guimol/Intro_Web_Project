8b7fbe83b203796a0f2ccf8d82ff88764328c23b
drakcamicazi <drak.milena@gmail.com>
2022-07-14 18:48:17 -0300

    configurando para deploy

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/get_list_tokens.vue';
-
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
   <app-header />
   <router-view />
   <HelloWorld msg="Hello to Dedos Ligeiros" />
+  <app-footer />
 </template>
 
 <style>
