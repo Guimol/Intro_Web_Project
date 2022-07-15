8945449f495a14c2d4ab21877cb6587c7560824a
Guilherme Oliveira <51828246+Guimol@users.noreply.github.com>
2022-07-15 07:59:20 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Intro_Web_Project/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "Intro_Web_Project/Intro_Web_Project/" : "/",
 };
