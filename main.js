03f998601bac243497d35cf006c38f3ca5d04d08
Guilherme Oliveira <51828246+Guimol@users.noreply.github.com>
2022-07-14 21:11:06 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from 'vue';
+import App from 'src/App.vue';
+
+createApp(App).mount('#app');
+
+import 'bootstrap/dist/js/bootstrap.js';
